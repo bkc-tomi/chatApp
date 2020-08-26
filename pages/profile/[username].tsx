0e87c8d529bf235b08d04b5e145f96fe868336e8
tomi <bkc.tomi1979@gmail.com>
2020-08-26 20:20:13 +0900

    fix username.tsx two times

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -104,7 +104,7 @@ export default function Profile() {
                 <div className={ Styles.copydiv }>
                     <div id="roomURL" className={ Styles.url }>
                         <BasicParagraph>
-                            { location.protocol　+ location.host + `/chatroom/${user.uid}` }
+                            { location.protocol + "://" + location.host + `/chatroom/${user.uid}` }
                         </BasicParagraph>
                     </div>
                     <div className={ Styles.copyBtn }>
